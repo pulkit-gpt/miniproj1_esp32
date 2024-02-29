6dead2ad84076ed0ecd46e87fbfd910a206766a0
pulkit-gpt <hiiampulkitg@gmail.com>
2024-02-29 17:07:05 +0530

    pwm pin

--- motor.py
+++ motor.py
@@ -1,19 +1,20 @@
-from machine import Pin
+from machine import Pin, PWM
 
 in3= Pin(15,Pin.OUT)
 in4= Pin(2,Pin.OUT)
-enb= Pin(0,Pin.OUT)
+enb= PWM(Pin(0), freq=5000,duty_u16=2**16-1)
 
 
 def Forward():
     in4.on()
     in3.off()
-    enb.on()
+    #enb.on()
+    enb.duty_u16(32768)
     return
 
 def Backward():
     in4.off()
     in3.on()
-    enb.on()
+    #enb.on()
     return
     
